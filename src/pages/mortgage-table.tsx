8dbbf5070979c4e4181701c907a82e122fcefa91
mathiastj <2278040+mathiastj@users.noreply.github.com>
2023-04-26 22:04:04 +0200

    Make table prettier

--- src/pages/mortgage-table.tsx
+++ src/pages/mortgage-table.tsx
@@ -3,33 +3,35 @@ import { CalculatedLoan } from './types'
 
 export default function MortgageTable({ calculatedLoan }: { calculatedLoan: CalculatedLoan }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>År</th>
-          <th>Restgæld</th>
-          <th>Renter</th>
-          <th>Afdrag</th>
-          <th>Bidrag</th>
-          <th>Ydelse før skat</th>
-          <th>Fradrag</th>
-          <th>Ydelse efter skat</th>
-        </tr>
-      </thead>
-      <tbody>
-        {[...Array(calculatedLoan.length)].map((_x, index) => (
-          <tr key={index}>
-            <td>{index + 1}</td>
-            <td>{calculatedLoan[index].principal}</td>
-            <td>{calculatedLoan[index].interest}</td>
-            <td>{calculatedLoan[index].instalment}</td>
-            <td>{calculatedLoan[index].extraCharge}</td>
-            <td>{calculatedLoan[index].pricePreTax}</td>
-            <td>{calculatedLoan[index].taxDeduction}</td>
-            <td>{calculatedLoan[index].pricePostTax}</td>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 border-separate border-spacing-x-4">
+        <thead>
+          <tr>
+            <th>År</th>
+            <th>Restgæld</th>
+            <th>Renter</th>
+            <th>Afdrag</th>
+            <th>Bidrag</th>
+            <th>Ydelse før skat</th>
+            <th>Fradrag</th>
+            <th>Ydelse efter skat</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {[...Array(calculatedLoan.length)].map((_x, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{calculatedLoan[index].principal.toFixed(0)}</td>
+              <td>{calculatedLoan[index].interest.toFixed(2)}</td>
+              <td>{calculatedLoan[index].instalment.toFixed(2)}</td>
+              <td>{calculatedLoan[index].extraCharge.toFixed(2)}</td>
+              <td>{calculatedLoan[index].pricePreTax.toFixed(2)}</td>
+              <td>{calculatedLoan[index].taxDeduction.toFixed(2)}</td>
+              <td>{calculatedLoan[index].pricePostTax.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
