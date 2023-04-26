2a53ce09889aa531d356c8ad1b4696adf94609e8
mathiastj <2278040+mathiastj@users.noreply.github.com>
2023-04-26 22:02:42 +0200

    Make page wider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-fit items-center justify-between font-mono text-sm lg:flex">
         <MortgageInput setCalculatedLoan={setCalculatedLoan} />
         <MortgageTable calculatedLoan={calculatedLoan} />
       </div>
