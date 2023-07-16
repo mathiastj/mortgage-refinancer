a8ca1b1f6d6371cd22785bf8da9da759e27408d7
mathiastj <2278040+mathiastj@users.noreply.github.com>
2023-07-16 19:13:48 +0200

    Add min interest of 0.5

--- src/components/mortgage-input.tsx
+++ src/components/mortgage-input.tsx
@@ -206,6 +206,7 @@ export default function MortgageInput({
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="1"
               step={0.5}
+              min={0.5}
               required
               defaultValue={defaultValues.interest}
             />
@@ -300,6 +301,7 @@ export default function MortgageInput({
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="4"
               step={0.5}
+              min={0.5}
               required
               defaultValue={defaultValues.interestNewLoan}
             />
