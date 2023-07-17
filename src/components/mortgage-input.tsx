cff1d42d29b17031575dc509b4d931e5d18c2d44
mathiastj <2278040+mathiastj@users.noreply.github.com>
2023-07-17 08:52:47 +0200

    Allow negative interest

--- src/components/mortgage-input.tsx
+++ src/components/mortgage-input.tsx
@@ -222,8 +222,8 @@ export default function MortgageInput({
               id="interest"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="1"
-              step={0.5}
-              min={0.5}
+              step={0.01}
+              min={-1}
               required
               defaultValue={defaultValues.interest}
             />
