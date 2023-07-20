6d38474a7cda558c5c4baee8cffb3b774a727622
mathiastj <2278040+mathiastj@users.noreply.github.com>
2023-07-20 11:59:42 +0200

    Add extra charges for all institutes

---
+++ src/lib/extra-charges.ts
@@ -0,0 +1,102 @@
+export const Institutes = {
+  JYSKE: 'Jyske Realkredit',
+  TOTALKREDIT: 'Totalkredit',
+  RD: 'Realkredit Danmark',
+  NORDEA: 'Nordea'
+} as const
+
+export type InstituesType = (typeof Institutes)[keyof typeof Institutes]
+
+export const EXTRA_CHARGE_LOAN_INTERVALS: {
+  [Key in InstituesType]: {
+    from: number
+    to: number
+    charge: number
+    instalmentFreeCharge: number
+  }[]
+} = {
+  // https://www.totalkredit.dk/siteassets/dokumenter/privat/prisblad/prisblad--privat.pdf
+  [Institutes.TOTALKREDIT]: [
+    {
+      from: 0,
+      to: 0.4,
+      charge: 0.45,
+      instalmentFreeCharge: 0.55
+    },
+    {
+      from: 0.4,
+      to: 0.6,
+      charge: 0.85,
+      instalmentFreeCharge: 1.15
+    },
+    {
+      from: 0.6,
+      to: 0.8,
+      charge: 1.2,
+      instalmentFreeCharge: 2.0
+    }
+  ],
+  // https://www.jyskebank.dk/privat/priser/bolig
+  [Institutes.JYSKE]: [
+    {
+      from: 0,
+      to: 0.4,
+      charge: 0.325,
+      instalmentFreeCharge: 0.475
+    },
+    {
+      from: 0.4,
+      to: 0.6,
+      charge: 0.8,
+      instalmentFreeCharge: 0.95
+    },
+    {
+      from: 0.6,
+      to: 0.8,
+      charge: 1.0,
+      instalmentFreeCharge: 2
+    }
+  ],
+  // https://www.nordea.dk/Images/144-119161/20170105bidragssatser_.pdf
+  [Institutes.NORDEA]: [
+    {
+      from: 0,
+      to: 0.4,
+      charge: 0.375,
+      instalmentFreeCharge: 0.525
+    },
+    {
+      from: 0.4,
+      to: 0.6,
+      charge: 0.825,
+      instalmentFreeCharge: 1.125
+    },
+    {
+      from: 0.6,
+      to: 0.8,
+      charge: 1.125,
+      instalmentFreeCharge: 1.825
+    }
+  ],
+  // https://rd.dk/PDF/Privat/prisblad-privat.pdf tillæg på 0,0500 %-point medregnet, så det er kvartalsvist og sammenligneligt med de andre institutter
+  [Institutes.RD]: [
+    {
+      from: 0,
+      to: 0.4,
+      charge: 0.3248,
+      instalmentFreeCharge: 0.4248
+    },
+    {
+      from: 0.4,
+      to: 0.6,
+      charge: 0.8748,
+      instalmentFreeCharge: 0.9748
+    },
+    {
+      from: 0.6,
+      to: 0.8,
+      charge: 1.4,
+      instalmentFreeCharge: 2.4
+    }
+  ]
+}
