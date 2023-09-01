ced693cf4fff297719f077f450a83830a2358ceb
mathiastj <2278040+mathiastj@users.noreply.github.com>
2023-09-01 20:09:01 +0200

    Hopefully fix copy button sometimes not working

--- src/components/mortgage-input.tsx
+++ src/components/mortgage-input.tsx
@@ -530,8 +530,8 @@ export default function MortgageInput({
           {copyLink && (
             <button
               type="button"
-              onClick={() => {
-                navigator.clipboard.writeText(copyLink)
+              onClick={async () => {
+                await navigator.clipboard.writeText(copyLink)
                 alert('Link kopieret til udklipsholder')
               }}
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
